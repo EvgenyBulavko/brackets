d0ca1142266703cc38fd11ddf0af113152d89952
Bulavko Evgeniy <z-bulavko@mail.ru>
2020-03-22 14:29:58 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let mass =[]
+  for (let i = 0; i < bracketsConfig.length;) {
+    mass = bracketsConfig[i].join('')
+    if (str.includes(mass)) {
+      str = str.replace(mass, '')
+      i = 0
+    } else {
+      i++
+    }
+  }
+  if (str) {
+    return false;
+} else {
+    return true;
+}
+
+
 }
